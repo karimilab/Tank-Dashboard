da75bbc20eb97a808a2a5a8cdd6600c31a22839e
damienleee <damienleee99@gmail.com>
2023-05-22 21:10:33 +0800

    test

--- test.py
+++ test.py
@@ -1,3 +1,8 @@
-from assimulo.problem import Implicit_Problem
-from assimulo.solvers import IDA
+import requests
 
+def visit_website():
+    url = 'https://cryotank-nus.streamlit.app'  # Replace with the URL of the website you want to visit
+    response = requests.get(url)
+    print(f"Website visited - Status code: {response.status_code}")
+
+visit_website()
