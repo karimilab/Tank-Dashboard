322f25dd64d94f3527a0326638174211654ac351
damienleee <damienleee99@gmail.com>
2023-06-25 14:10:14 +0800

    test

--- test.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-def visit_website():
-    url = 'https://cryotank-nus.streamlit.app'  # Replace with the URL of the website you want to visit
-    response = requests.get(url)
-    print(f"Website visited - Status code: {response.status_code}")
-
-visit_website()
